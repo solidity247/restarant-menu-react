3d1c61b216e7a765ece077a356082d94cf3411a7
Akcholpon Usenbekova <akcholponu@gmail.com>
2023-01-10 00:09:22 -0800

    added setActiveTab to visit page

--- restaurant-team-project-react/src/Components/CustomersAccess.jsx
+++ restaurant-team-project-react/src/Components/CustomersAccess.jsx
@@ -7,52 +7,64 @@ import VisitPage from "./VisitPage";
 import "./CustomersAccess.css";
 import CartPage from "./CartPage";
 
-
 const headerTabs = [
   {
-    label : "HOME",
-    id: 1
+    label: "HOME",
+    id: 1,
   },
   {
-    label : "MENU",
-    id: 2
+    label: "MENU",
+    id: 2,
   },
   {
-    label : "VISIT US",
-    id: 3
+    label: "VISIT US",
+    id: 3,
   },
   {
     label: "Cart",
-    id: 4
-  }
-]
+    id: 4,
+  },
+];
 
-export default function CustomersAccess({ menu, setMenu, setAccess}) {
-
-  const [cartItems, setCartItems] = useState([])
-  const [activeTab, setActiveTab] = useState(0)
-  const [numberOfOrders, setNumberOfOrders] = useState(0)
-  useEffect(()=>{
-    const fromStorage = JSON.parse(localStorage.getItem("cart"))
-    if(fromStorage.length){
-      setCartItems(fromStorage)
+export default function CustomersAccess({ menu, setMenu, setAccess }) {
+  const [cartItems, setCartItems] = useState([]);
+  const [activeTab, setActiveTab] = useState(0);
+  const [numberOfOrders, setNumberOfOrders] = useState(0);
+  useEffect(() => {
+    const fromStorage = JSON.parse(localStorage.getItem("cart"));
+    if (fromStorage.length) {
+      setCartItems(fromStorage);
     }
-  },[])
-  useEffect(()=>{
-    localStorage.setItem("cart", JSON.stringify(cartItems))
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cartItems));
     // const calculatedOrders = cartItems.reduce((acc, item)=> acc+item.inCart, 0)
     // setNumberOfOrders(calculatedOrders);
-    console.log(cartItems)
-  },[cartItems])
+    console.log(cartItems);
+  }, [cartItems]);
 
   return (
     <div className="customer-page-container">
-      <Header activeTab={activeTab} cartItems={cartItems} setActiveTab={setActiveTab} headerTabs={headerTabs} numberOfOrders={numberOfOrders}/>
-      {activeTab === 0 && <HomePage/>}
-      {activeTab === 1 && <MenuPage menu = {menu} setMenu={setMenu} cartItems= {cartItems} setCartItems={setCartItems}/>}
-      {activeTab === 2 && <VisitPage/>}
-      {activeTab === 3 && <CartPage cartItems={cartItems} setCartItems={setCartItems}/>}
-      
+      <Header
+        activeTab={activeTab}
+        cartItems={cartItems}
+        setActiveTab={setActiveTab}
+        headerTabs={headerTabs}
+        numberOfOrders={numberOfOrders}
+      />
+      {activeTab === 0 && <HomePage />}
+      {activeTab === 1 && (
+        <MenuPage
+          menu={menu}
+          setMenu={setMenu}
+          cartItems={cartItems}
+          setCartItems={setCartItems}
+        />
+      )}
+      {activeTab === 2 && <VisitPage setActiveTab={setActiveTab} />}
+      {activeTab === 3 && (
+        <CartPage cartItems={cartItems} setCartItems={setCartItems} />
+      )}
       <br />
       And much more from customer's site
       <br />
