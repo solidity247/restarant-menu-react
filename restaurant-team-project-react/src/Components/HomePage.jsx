0741a1d31c64a72205937bfc5a78a6af63db2ade
Diana <106848567+Diflorens@users.noreply.github.com>
2023-01-13 01:34:06 +0100

    css details

--- restaurant-team-project-react/src/Components/HomePage.jsx
+++ restaurant-team-project-react/src/Components/HomePage.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import Slideshow from './Slideshow'
 import AboutUs from './AboutUs'
+import { Button } from '@mui/material'
+import ReserveModal from './ReserveModal';
+import './ReserveModal'
 
 
 
 function HomePage() {
+  const [openR, setOpenR] = React.useState(false);
   return (
     <div>
+      {openR && <ReserveModal openR = {openR}
+      setOpenR = {setOpenR}/>}
       <Slideshow />
+     <Button
+     id='main-reserve'
+     onClick={()=>setOpenR(true)}
+     >Reservation</Button>
+     <br />
+     <br />
       <AboutUs />
     </div>
   )
